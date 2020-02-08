e326b57f9af36d16c79b65a3e587627765968950
Clayton Carney <clayton.carney@gmail.com>
2020-02-08 16:33:56 -0700

    Def Sub Change 2

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ let appPrefix = 'kjs-test';
 
 let appCaches = [
   {
-    name: 'kjs-test-core-20200208.05',
+    name: 'kjs-test-core-20200208.10',
     urls: [
       './',
       './bundle.js',
@@ -34,7 +34,7 @@ let appCaches = [
     ]
   },
   {
-    name: 'kjs-test-help-20200208.05',
+    name: 'kjs-test-help-20200208.10',
     urls: [
       './help/about.html',
       './help/bookmark.html',
