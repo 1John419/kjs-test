24182851627cc2b2a292897bc85b98d0abf1bba3
Clayton Carney <clayton.carney@gmail.com>
2020-04-18 14:35:42 -0600

    Cache Update

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ let appPrefix = 'kjs-test';
 
 let appCaches = [
   {
-    name: 'kjs-test-core-20200418.05',
+    name: 'kjs-test-core-20200418.05.02',
     urls: [
       './',
       './bundle.js',
@@ -16,7 +16,7 @@ let appCaches = [
     ]
   },
   {
-    name: 'kjs-test-css-20200207.01',
+    name: 'kjs-test-css-20200418.01',
     urls: [
       './css/kjs.css',
       './css/font.css'
