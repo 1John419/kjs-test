dd28310f8869d530505a7b05df1898e364ebfbf5
Clayton Carney <clayton.carney@gmail.com>
2020-07-15 10:43:11 -0600

    Strong Corrections

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ let appPrefix = 'kjs-test';
 
 let appCaches = [
   {
-    name: 'kjs-test-core-20200511.01',
+    name: 'kjs-test-core-20200715.01',
     urls: [
       './',
       './bundle.js',
@@ -16,7 +16,7 @@ let appCaches = [
     ]
   },
   {
-    name: 'kjs-test-css-20200523.01',
+    name: 'kjs-test-css-20200715.01',
     urls: [
       './css/kjs.css',
       './css/font.css'
@@ -34,7 +34,7 @@ let appCaches = [
     ]
   },
   {
-    name: 'kjs-test-help-20200523.01',
+    name: 'kjs-test-help-20200715.01',
     urls: [
       './help/about.html',
       './help/bookmark.html',
@@ -49,7 +49,7 @@ let appCaches = [
     ]
   },
   {
-    name: 'kjs-test-json-20200418.01',
+    name: 'kjs-test-json-20200715.01',
     urls: [
       './json/strong.json',
       './json/tome.kjv.json'
